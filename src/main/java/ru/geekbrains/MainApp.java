4421fd223253703c317d274ef1dce0d33d579e51
maga <saievmuhammad@mail.ru>
2021-05-12 05:55:07 +0300

    this is homework from lesson ReflectionAPI

--- src/main/java/ru/geekbrains/MainApp.java
+++ src/main/java/ru/geekbrains/MainApp.java
@@ -1,4 +1,7 @@
 package ru.geekbrains;
 
 public class MainApp {
+    public static void main(String[] args) {
+        StartTest.start(StartTest.class);
+    }
 }
--- src/main/java/ru/geekbrains/MyTest.java
+++ src/main/java/ru/geekbrains/MyTest.java
@@ -1,4 +1,33 @@
 package ru.geekbrains;
 
 public class MyTest {
+    @BeforeSuite
+    public static void test1() {
+        System.out.println("test2 BeforeSuite");
+    }
+
+    @Test
+    public static void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+    public static void test3() {
+        System.out.println("test3");
+    }
+
+    @Test(priority = 2)
+    public static void test4() {
+        System.out.println("test4 priority = 2");
+    }
+
+    @Test(priority = 7)
+    public static void test5() {
+        System.out.println("test5 priority = 7");
+    }
+
+    @AfterSuite
+    public static void test6() {
+        System.out.println("test6 AfterSuite");
+    }
 }
